bfea3d563c28f6cdfb229b0d2cdd3aefed158a66
molo650 <langiejeroen9050@gmail.com>
2021-10-28 09:24:03 +0200

    Adres uiteenhalen

--- BusinessLayer/Entities/Bestuurder.cs
+++ BusinessLayer/Entities/Bestuurder.cs
@@ -16,11 +16,18 @@ namespace Bussiness_Layer.Entities {
 
         public string Naam { get; private set; }
         public string Voornaam { get; private set; }
-        public string Adres { get; private set; }
+        public int Postcode { get; private set; }
+        public string Gemeente { get; private set; }
+        public string Straat { get; private set; }
+        public string Huisnummer { get; private set; }
         public DateTime GeboorteDatum { get; private set; }
         public string RijksregisterNummer { get; private set; }
         public string Rijbewijs { get; private set; }
         public Voertuig Voertuig { get; private set; }
         public Tankkaart Tankkaart { get; private set; }
+
+        public void Update() {
+
+        }
     }
 }
